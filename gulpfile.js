7d8f4a0130d9bb56de15de7f0d0c479817bea0bc
Andy Alderton <andrewalderton@sky.com>
2017-10-06 14:43:59 +0100

    Update image compression

--- gulpfile.js
+++ gulpfile.js
@@ -134,7 +134,17 @@ gulp.task('optimize-image', function(done) {
 gulp.task('compress-image', function(done) {
   gulp.src('src/img/*')
     .pipe(plumber())
-    .pipe(imagemin())
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]))
     .pipe(gulp.dest('dist/img'));
     done();
 });
